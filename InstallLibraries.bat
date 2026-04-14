aab416c1ce9afd07ff7a222c062e6d678f0b1541
tueftelpark-jamil <jamil.koller@2point.ch>
2026-04-14 09:42:22 +0200

    extend bat script to empty libraries folder before installing the new libraries

--- InstallLibraries.bat
+++ InstallLibraries.bat
@@ -12,16 +12,29 @@ set "TEMP_ZIP=%TEMP%\ArduinoLibsRepo.zip"
 set "TEMP_EXTRACT=%TEMP%\ArduinoLibsExtract"
 set "ARDUINO_LIB_PATH=%USERPROFILE%\Documents\Arduino\libraries"
 
-:: Sicherstellen, dass der Arduino-Library-Ordner existiert
-if not exist "%ARDUINO_LIB_PATH%" (
-    echo [INFO] Erstelle Arduino Library Ordner...
-    mkdir "%ARDUINO_LIB_PATH%"
+:: Sicherheitswarnung vor dem Loeschen
+echo [ACHTUNG] Der folgende Ordner wird jetzt komplett geleert:
+echo           -^> %ARDUINO_LIB_PATH%
+echo.
+echo           Alle darin enthaltenen, bisherigen Libraries gehen verloren!
+echo           Schliesse dieses Fenster oder druecke STRG+C zum Abbrechen.
+echo           Um fortzufahren und alles zu ueberschreiben...
+pause
+
+:: Zielordner leeren und neu erstellen
+if exist "%ARDUINO_LIB_PATH%" (
+    echo.
+    echo [INFO] Leere den bestehenden Arduino Library Ordner...
+    rmdir /S /Q "%ARDUINO_LIB_PATH%"
 )
+echo [INFO] (Neu-)Erstellung des Arduino Library Ordners...
+mkdir "%ARDUINO_LIB_PATH%"
 
 :: Alten temporaeren Entpack-Ordner leeren, falls er noch existiert
 if exist "%TEMP_EXTRACT%" rmdir /S /Q "%TEMP_EXTRACT%"
 
 :: 1. Herunterladen (nutzt das in Windows integrierte 'curl')
+echo.
 echo [1/4] Lade das Repository von GitHub herunter...
 curl -L -s -o "%TEMP_ZIP%" "%REPO_URL%"
 if %errorlevel% neq 0 (
@@ -49,7 +62,8 @@ if exist "%SOURCE_LIBS%" (
     echo.
     echo ========================================================
     echo   Installation ERFOLGREICH abgeschlossen!
-    echo   Alle Libraries wurden in folgenden Ordner kopiert:
+    echo   Alle alten Libraries wurden entfernt und die neuen
+    echo   wurden in folgenden Ordner kopiert:
     echo   %ARDUINO_LIB_PATH%
     echo ========================================================
 ) else (
